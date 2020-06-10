334ee0a5bcda913bda429c3fdd26074a6ff0d670
error-listen <danielsilva0.8892@hotmail.com>
2020-06-10 00:27:20 -0300

    First Commit

--- src/server.js
+++ src/server.js
@@ -15,6 +15,11 @@ mongoose.connect(
     process.env.MONGO_URL,
     { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
 
+
+app.use(cors())
+app.use(express.json())
+app.use(routes)
+
 const connected_users = {}
 
 io.on('connection', socket => {
@@ -29,10 +34,6 @@ app.use((req, res, next) => {
 
     next()
 })
-
-app.use(cors())
-app.use(express.json())
-app.use(routes)
 
 server.listen(process.env.PORT || 5000, () => {
     console.log('Running server on port 4000')
