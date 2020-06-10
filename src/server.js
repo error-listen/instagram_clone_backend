46c0a2023f5231239df5db9cc48601d84a0fd63e
error-listen <danielsilva0.8892@hotmail.com>
2020-06-10 00:14:10 -0300

    First Commit

--- src/server.js
+++ src/server.js
@@ -8,27 +8,12 @@ const routes = require('./routes')
 
 const app = express()
 
-const server = require('http').Server(app)
+const server = require('http').Server(app, { origins: '*:*'})
 const io = require('socket.io')(server)
 
 mongoose.connect(
     process.env.MONGO_URL,
     { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
-
-const allowCrossDomain = function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*')
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With')
-
-    if ('OPTIONS' == req.method) {
-        res.send(200)
-    }
-    else {
-        next()
-    }
-}
-
-app.use(allowCrossDomain)
 
 const connected_users = {}
 
